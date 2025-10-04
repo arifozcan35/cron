f38c64896b39b6c44a634d5f31d9e3cfbc19451b
Arif Özcan <zcanarif@gmail.com>
2025-10-04 12:28:19 +0300

    first commit

---
+++ src/test/java/tr/cronjob/CronJobApplicationTests.java
@@ -0,0 +1,13 @@
+package tr.cronjob;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CronJobApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
